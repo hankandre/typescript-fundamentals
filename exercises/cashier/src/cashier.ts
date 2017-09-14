9ecd7f85e6f3cdb6dfa2540afe9d382013d87098
Hank Andre <hankandre@gmail.com>
2017-09-14 15:38:17 -0500

    completed assignment

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,3 +1,37 @@
-export function cashier() {
-  return {};
+interface Item {
+  name: string;
+  price: number;
+  qty: number;
+}
+
+interface CashierAPI {
+  addItem: (item: Item) => CashierAPI;
+  add: (name: string, price: number, qty?: number) => CashierAPI;
+  length: number;
+  total: number;
+}
+
+export function cashier(): CashierAPI {
+  let items: Item[] = [];
+  return {
+    get length() {
+      return items.reduce((tot, curr) => {
+        return tot + curr.qty;
+      }, 0);
+    },
+    add(name, price, qty = 1) {
+      return this.addItem({ name, price, qty });
+    },
+    addItem(item: Item) {
+      items.push(item);
+      return this;
+    },
+    get total() {
+      return (
+        items.reduce((tot, curr) => {
+          return Math.round(tot + curr.price * curr.qty * 100);
+        }, 0) * 0.01
+      );
+    }
+  };
 }
